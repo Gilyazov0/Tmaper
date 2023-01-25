33fa6c41d5359eca2f323c99a4be299b4ddcec74
Barak Kalfa <BarakK@cnoga.local>
2023-01-25 12:05:13 +0200

    1.1

--- tmapper/src/App.js
+++ tmapper/src/App.js
@@ -1,11 +1,8 @@
 import CompanyStructure from "./Components/CompanyStructure";
+import CompaniesDB from "./CompaniesDB.json";
 
 function App() {
-  return (
-    <div className="App">
-      <CompanyStructure />
-    </div>
-  );
+   return <div className="App">{<CompanyStructure />}</div>;
 }
 
 export default App;
