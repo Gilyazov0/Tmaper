96276acdc45fb5cc2fe7a13222cb8036a037ed3a
Sergey Gilyazov <s.gilyazov@gmail.com>
2023-01-25 13:34:03 +0200

    working

--- tmapper/src/connection/GetCompanyData.js
+++ tmapper/src/connection/GetCompanyData.js
@@ -1,8 +1,8 @@
-import CompaniesData from "./CompaniesDB";
+import { companiesData } from "./CompaniesDB";
 import axios from "axios";
 
 export default async function getCompanyData(companyId = 1) {
   const res = axios.get("http://xxxxxxx/companyId");
 
-  return CompaniesData;
+  return companiesData;
 }
