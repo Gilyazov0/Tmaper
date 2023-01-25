f9f614d1cce1754857a2c8f9a79eacfdfd3cb8ec
Raphaelle1 <mizrahiraphaelle@gmail.com>
2023-01-25 17:46:05 +0200

    done navbar

--- tmapper/src/Navbar-slide/Navbar.js
+++ tmapper/src/Navbar-slide/Navbar.js
@@ -1,8 +1,8 @@
 import React, {useState} from 'react';
 import hamburger from "./list.png";
 import "./Navbar.css";
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
-import { Navbar, Nav } from 'react-bootstrap';
+import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
+import {Navbar, Nav} from 'react-bootstrap';
 
 
 function Menu() {
@@ -14,24 +14,37 @@ function Menu() {
 
     return (
         <BrowserRouter>
-                    <Navbar expand="md" className={`navbar ${isOpen ? 'is-open' : 'is-closed'}`}>
-                <img src={hamburger} alt="hamburger" className="hamburger" onClick={toggleNavbar} />
-                <Nav className="ml-auto" navbar>
-                    <Link to="/" className="nav-link">Home</Link>
-                    <Link to="/employee" className="nav-link">Employee Profil</Link>
-                    <Link to="/contact" className="nav-link">Contact</Link>
-                </Nav>
-            </Navbar>
-            <div className={`content ${isOpen ? 'is-open' : 'is-closed'}`}>
-                {/* <Routes>
+            <div>
+                <img src={hamburger}
+                    alt="hamburger"
+                    className="hamburger"
+                    onClick={toggleNavbar}/>
+                <Navbar expand="md"
+                    className={
+                        `navbar ${
+                            isOpen ? 'is-open' : 'is-closed'
+                        }`
+                }>
+
+                    <Nav className="link-name" navbar>
+                        <Link to="/" className="nav-link-home">Home</Link>
+                        <Link to="/employee" className="nav-link-employee">Employee Profil</Link>
+                        <Link to="/contact" className="nav-link-contact">Contact</Link>
+                    </Nav>
+                </Navbar>
+                <div className={
+                    `content ${
+                        isOpen ? 'is-open' : 'is-closed'
+                    }`
+                }>
+                    {/* <Routes>
                     <Route path="/" element={<Home/>} />
                     <Route path="/employee" element={<Employee/>} />
                     <Route path="/contact" element={<Contact/>}/>
-                </Routes>  */}
+                </Routes>  */} </div>
             </div>
         </BrowserRouter>
     );
 }
 
 export default Menu;
-
